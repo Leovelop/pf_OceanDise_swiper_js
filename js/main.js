5993fb87767f85c9c6b2050a855206f9d5c77eb3
Leovelop <saintiscah@gmail.com>
2022-09-22 22:59:15 +0900

    정렬

--- js/main.js
+++ js/main.js
@@ -1,47 +1,47 @@
 const btnPlay = document.querySelector(".play");
 const btnPause = document.querySelector(".pause");
 
-let swiper = new Swiper("#wrap", { 
-    direction: "horizontal",
-    spaceBetween: 50,
-    loop: true,
-    slidesPerView: "auto",
-    centeredSlides: true,
-    speed: 1000,
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev"
-    },
-    autoplay: {
-        delay: 3000,
-        disableOnInteraction: false,
-    },
-    mousewheel: true,
-    effect: 'coverflow',
-    grabCursor: true,
-    coverflowEffect: {
-        rotate: 50,
-        stretch: 30,
-        depth: 200,
-        modifier: 1,
-        slideShadows: true,
-    }
+let swiper = new Swiper("#wrap", {
+  direction: "horizontal",
+  spaceBetween: 50,
+  loop: true,
+  slidesPerView: "auto",
+  centeredSlides: true,
+  speed: 1000,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  },
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  mousewheel: true,
+  effect: 'coverflow',
+  grabCursor: true,
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 30,
+    depth: 200,
+    modifier: 1,
+    slideShadows: true,
+  }
 });
 
 swiper.autoplay.stop();
 
-btnPlay.onclick = function(){
-    swiper.autoplay.start();
-    btnPause.classList.remove("on");
-    this.classList.add("on");
+btnPlay.onclick = function () {
+  swiper.autoplay.start();
+  btnPause.classList.remove("on");
+  this.classList.add("on");
 }
 
-btnPause.onclick = function(){
-    swiper.autoplay.stop();
-    btnPlay.classList.remove("on");
-    this.classList.add("on");
+btnPause.onclick = function () {
+  swiper.autoplay.stop();
+  btnPlay.classList.remove("on");
+  this.classList.add("on");
 }
